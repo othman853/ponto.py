65d45e57204ef4599e89187c16fd1398db003e5d
othman853 <anuar.yasser@gmail.com>
2016-12-23 15:46:30 -0200

    Saving the entries on a CSV

--- lib/main.py
+++ lib/main.py
@@ -1,1 +1,13 @@
-print('Oi')
+import datetime
+
+APPEND_MODE = 'a'
+
+entry_datetime = datetime.datetime.now()
+today = entry_datetime.strftime('%Y-%m-%d')
+now = entry_datetime.strftime('%H:%M:%S')
+
+entries = open('./entries', APPEND_MODE)
+entries.write('{};{}\n'.format(today, now))
+entries.close()
+
+print('{}@{}'.format(today, now) )
